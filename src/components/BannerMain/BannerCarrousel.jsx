df8e110815fbbd68bf47c9737638a49bf868d6a2
Luis Torrealba <luisdavid25torrealba@gmail.com>
2023-03-29 23:37:11 -0400

    Trabajo en casa

--- src/components/BannerMain/BannerCarrousel.jsx
+++ src/components/BannerMain/BannerCarrousel.jsx
@@ -12,7 +12,7 @@ const BannerCarrousel = ({banner, component}) => {
     <section className='Carrousel__Banner__Desktop'>
       <Carousel fade indicators={false}>
           {banner?.map((banner, index) => (
-            <Carousel.Item interval={2000} key={`${component}-${'desktop'}-${index}`} >
+            <Carousel.Item interval={4000} key={`${component}-${'desktop'}-${index}`} >
             <img
               className="d-block w-100"
               src={banner.banner__desktop}
@@ -28,9 +28,9 @@ const BannerCarrousel = ({banner, component}) => {
     </section>
     {/* Banner Desktop */}
     <section className='Carrousel__Banner__Movil'>
-      <Carousel fade>
+      <Carousel fade indicators={false} >
           {banner?.map((banner, index) => (
-            <Carousel.Item interval={2000} key={`${component}-${'movil'}-${index}`}>
+            <Carousel.Item interval={4000} key={`${component}-${'movil'}-${index}`}>
             <img
               key={`${'BannerCarrousel'}-${index}`}
               className="d-block w-100"
